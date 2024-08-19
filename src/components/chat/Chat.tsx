171302bb522cf42d39ec5ab2a62d783b2c60e0f2
eunhye han <hellou8363@gmail.com>
2024-08-19 11:05:48 +0900

    Fix: 16인치 화면에서 채팅창이 footer에 가려지는 부분 수정

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -157,7 +157,7 @@ const Chat = ({ subjectId, targetId, roomId, handler }: Chat) => {
         <IconX stroke={2} onClick={() => webSocketDisConnection()} />
       </Group>
       {isTargetEntered && `${targetId}님이 입장했습니다.`}
-      <ScrollArea h={500} mt={20} pr={20} mb={20} scrollbars="y" viewportRef={viewport}>
+      <ScrollArea h={400} mt={20} pr={20} mb={20} scrollbars="y" viewportRef={viewport}>
         {prevChatHistory.map((value, index) =>
           value.subjectId === subjectId ? (
             <Group key={index} align="end" justify="flex-end" mt={10} mb={10}>
